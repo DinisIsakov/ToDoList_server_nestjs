b02b130f4bb4a8636b5580ed58ee99d7c81c20a6
Dinis Isakov <isakovdr@gmail.com>
2023-07-31 09:43:07 +0300

    create server todolist

---
+++ src/todos/todo.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Header,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { CreateTodo } from './dto/create-todo.dto';
+import { ChangeTodo } from './dto/change-todo.dto';
+
+@Controller('todos')
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Get()
+  getAllTodods() {
+    return this.todoService.findAll();
+  }
+
+  @Get(':id')
+  getOneTodo(@Param('id') id: string) {
+    return this.todoService.findOne(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @Header('Content-Type', 'application/json')
+  createTodo(@Body() createTodo: CreateTodo) {
+    return this.todoService.create(createTodo);
+  }
+
+  @Patch(':id')
+  cgangeTodo(@Body() changeTodo: ChangeTodo, @Param('id') id: string) {
+    return this.todoService.update(id, changeTodo);
+  }
+
+  @Delete(':id')
+  deleteTodo(@Param('id') id: string) {
+    return this.todoService.remove(id);
+  }
+}
